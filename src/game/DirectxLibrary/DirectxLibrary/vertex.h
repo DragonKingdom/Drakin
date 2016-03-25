2bc0b71ce75f0585b886c33ad36d59152e6da915
Moto0116 <moto01160902@gmail.com>
2016-03-25 18:31:47 +0900

    vertexクラスに関数追加
    道の敷設のために必要な関数を追加した

--- src/game/DirectxLibrary/DirectxLibrary/vertex.h
+++ src/game/DirectxLibrary/DirectxLibrary/vertex.h
@@ -3,7 +3,8 @@
 
 class Texture;
 
-class Vertex {
+class Vertex 
+{
 public:
 	typedef struct FRECT
 	{
@@ -69,6 +70,7 @@ public:
 	void DrawTexture( float _x, float _y, D3DCOLOR _color = D3DCOLOR_ARGB(255,255,255,255));
 	void DrawTexture( float _x, float _y, FRECT _rect, D3DCOLOR _color = D3DCOLOR_ARGB(255,255,255,255));
 
+	void VertexDraw( Texture _texture, D3DXVECTOR3* _pVec, D3DCOLOR _color = D3DCOLOR_ARGB(255,255,255,255) );
 
 public:
 	/// X•ûŒüEY•ûŒü‚Ì”{—¦‚ğ“¯‚É•ÏX
