1d2ca82e771070fd7ccec9515b1da0bae86b4e34
Keitaro0408 <kei.kotani77@gmai.com>
2016-06-28 23:44:42 +0900

    途中経過 座標変換の設定をどうしようか悩み中

--- src/game/DirectxLibrary/DirectxLibrary/ShaderAssist.cpp
+++ src/game/DirectxLibrary/DirectxLibrary/ShaderAssist.cpp
@@ -1,18 +1,73 @@
 #include "stdafx.h"
 #include "ShaderAssist.h"
+#include "graphicsDevice.h"
 
 
-ShaderAssist::ShaderAssist()
+ShaderAssist::ShaderAssist(char* _pFxFileName)
+	:m_pDevice(GraphicsDevice::getInstance().GetDevice()),
+	pFxFileName(_pFxFileName)
 {
-	D3DCAPS9 caps;
-
 }
 
 ShaderAssist::~ShaderAssist()
 {
 }
 
-void ShaderAssist::GetParameterHandle()
+D3DXHANDLE ShaderAssist::GetParameterHandle(const char* _paramName)
 {
+	/// @todo 今、NULLを指定しているが多分必要になると思うので、その時修正する 
+	return m_pEffect->GetParameterByName(NULL, _paramName);
+}
 
+void ShaderAssist::Begin()
+{
+	if (m_pEffect)
+	{
+		m_pDevice->GetTransform(D3DTS_VIEW, &m_matView);
+		m_pDevice->GetTransform(D3DTS_PROJECTION, &m_matProj);
+
+		m_pEffect->Begin(NULL, 0);
+	}
+}
+
+void ShaderAssist::BeginPass(UINT Pass)
+{
+	if (m_pEffect)
+	{
+		m_pDevice->GetTransform(D3DTS_VIEW, &m_matView);
+		m_pDevice->GetTransform(D3DTS_PROJECTION, &m_matProj);
+
+		m_pEffect->Begin(NULL, 0);
+	}
+}
+
+void ShaderAssist::SetMatrix(D3DXMATRIX* pMatWorld)
+{
+	D3DXMATRIX matrix;
+	matrix = (*pMatWorld) * m_matView * m_matProj;
+}
+
+HRESULT ShaderAssist::LoadTechnique(const char* _entryPoint)
+{
+	D3DCAPS9 caps;
+
+	//シェーダーのバージョンが使えるかチェック
+	m_pDevice->GetDeviceCaps(&caps);
+	if (caps.VertexShaderVersion >= D3DVS_VERSION(1, 1) && caps.PixelShaderVersion >= D3DPS_VERSION(2, 0))
+	{
+		LPD3DXBUFFER pErr = NULL;
+		if (FAILED(D3DXCreateEffectFromFile(m_pDevice, pFxFileName, NULL, NULL, 0, NULL, &m_pEffect, &pErr)))
+		{
+			MessageBox(NULL, "fxファイルの読み込みに失敗しました。", "エラー", MB_OK);
+			return -1;
+		}
+		m_pTechnique = m_pEffect->GetTechniqueByName("TShader");
+		m_pEffect->SetTechnique(m_pTechnique);
+	}
+	else
+	{
+		MessageBox(NULL, "グラフィックボードが対応していません。", "エラー", MB_OK);
+		return -1;
+	}
+	return S_OK;
 }
--- src/game/DirectxLibrary/DirectxLibrary/ShaderAssist.h
+++ src/game/DirectxLibrary/DirectxLibrary/ShaderAssist.h
@@ -1,17 +1,24 @@
 #ifndef SHADERASSIST_H
 #define SHADERASSIST_H
 #include <d3dx9.h>
-
 class ShaderAssist
 {
-public:
-	ShaderAssist();
-	~ShaderAssist();
-	void GetParameterHandle();
 private:
 	LPDIRECT3DDEVICE9	m_pDevice;
-	LPD3DXEFFECT m_pEffect;
-	const D3DXVECTOR4* pCameraPos;
+	LPD3DXEFFECT		m_pEffect;
+	const D3DXVECTOR4*  pCameraPos;
+	//fxファイルのハンドル
+	D3DXHANDLE			m_pTechnique, m_pWVPP;
+	D3DXMATRIX m_matView, m_matProj;
+	char* pFxFileName;
+public:
+	ShaderAssist(char* _pFxFileName);
+	~ShaderAssist();
+	D3DXHANDLE GetParameterHandle(const char* _paramName);
+	HRESULT LoadTechnique(const char* _entryPoint);
+	void SetMatrix(D3DXMATRIX* pMatWorld);
+	void Begin();
+	void BeginPass(UINT Pass);
 
 };
 #endif
